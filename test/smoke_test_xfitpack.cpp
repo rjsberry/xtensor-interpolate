22b7df22622b2637ed517feb504c512691c1d7c5
Richard Berry <rjsberry@protonmail.com>
2018-03-10 11:35:25 +0000

    Refactor smoke tests to actually verify interpolation

--- test/smoke_test_xfitpack.cpp
+++ test/smoke_test_xfitpack.cpp
@@ -9,30 +9,45 @@
 
 // Smoke tests for fitpack routines: generally just check they run safely.
 
+#include <cmath>
+#include <vector>
+
 #include "gtest/gtest.h"
 
+#include "xtensor/xmath.hpp"
 #include "xtensor/xtensor.hpp"
 #include "xtensor/xrandom.hpp"
 
 #include "xtensor-fitpack/xfitpack.hpp"
 
-const std::size_t TEST_ARRAY_LENGTH = 100;
-
-TEST(xfitpack_smoke_test, splrep)
+namespace xt
 {
-    xt::xtensor<double, 1> x = xt::arange<double>(TEST_ARRAY_LENGTH);
-    xt::xtensor<double, 1> y = xt::random::randn<double>({TEST_ARRAY_LENGTH});
 
-    auto tck = xt::fitpack::splrep(x, y);
+TEST(smoke_test, cubic_bspline_interpolation)
+{
+    const std::size_t orig_arr_len = 100;
+    const std::size_t test_arr_len = 1000;
+
+    std::vector<std::function<xtensor<double, 1> (xtensor<double, 1>)>> tests =
+    {
+        [](auto x){ return sin(x); },
+        [](auto x){ return cos(x); },
+        [](auto x){ return x * x * x; },
+        [](auto x){ return 1/(1 + 25*x*x); }
+    };
+
+    for (const auto& f : tests)
+    {
+        xtensor<double, 1> x = linspace<double>(-M_PI, M_PI, orig_arr_len);
+        xtensor<double, 1> y = f(x);
+
+        auto tck = fitpack::splrep(x, y);
+
+        xtensor<double, 1> x_interp = linspace<double>(-M_PI, M_PI, test_arr_len);
+        auto y_interp = fitpack::splev(x_interp, tck);
+
+        EXPECT_TRUE(isclose(y_interp, f(x_interp))());
+    }
 }
 
-TEST(xfitpack_smoke_test, splev)
-{
-    xt::xtensor<double, 1> x = xt::arange<double>(TEST_ARRAY_LENGTH);
-    xt::xtensor<double, 1> y = xt::random::randn<double>({TEST_ARRAY_LENGTH});
-
-    auto tck = xt::fitpack::splrep(x, y);
-
-    xt::xtensor<double, 1> x_interp = xt::arange<double>(1, TEST_ARRAY_LENGTH-1) - 0.5;
-    auto y_interp = xt::fitpack::splev(x_interp, tck);
-}
+}  // namespace xt
