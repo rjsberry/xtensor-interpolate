6ac16b84e52076adde0db28142985a6982fd349e
Richard Berry <rjsberry@protonmail.com>
2018-03-14 18:58:20 +0000

    Add Doxygen-ready docstrings to UnivariateSpline and splev

--- include/xtensor-interpolate/xinterpolate.hpp
+++ include/xtensor-interpolate/xinterpolate.hpp
@@ -1,14 +1,14 @@
 #ifndef INCLUDE_XTENSOR_INTERPOLATE_HPP_
 #define INCLUDE_XTENSOR_INTERPOLATE_HPP_
 
-// xtensor-interpolate: https://github.com/rjsberry/xtensor-interpolate
-//
-// Copyright (C) 2018, Richard Berry <rjsberry@protonmail.com>
-//
-// Distributed under the terms of BSD 2-Clause "simplified" license. (See
-// accompanying file LICENSE, or copy at
-// https://github.com/rjsberry/xtensor-interpolate/blob/master/LICENSE)
-//
+/// xtensor-interpolate: https://github.com/rjsberry/xtensor-interpolate
+///
+/// Copyright (C) 2018, Richard Berry <rjsberry@protonmail.com>
+///
+/// Distributed under the terms of BSD 2-Clause "simplified" license. (See
+/// accompanying file LICENSE, or copy at
+/// https://github.com/rjsberry/xtensor-interpolate/blob/master/LICENSE)
+///
 
 #include <map>
 #include <string>
@@ -52,25 +52,25 @@ auto fpcurf0(xtensor<double, 1>& x, xtensor<double, 1>& y, xtensor<double, 1>& w
     return ier;
 }
 
-// Evaluate a B-Spline or its derivatives.
-//
-// @param [in] x
-//     An array of points at which to return the value of the smoothed
-//     spline or its derivatives.
-// @param [in] tck
-//     The tuple returned by `splrep` containing the knots, coefficients,
-//     and degree of the spline.
-// @param [in] der
-//     The order of derivative of the spline to compute (must be less than
-//      or equal to k).
-// @param [in] ext
-//     Controls the value returned for elements of `x` not in the interval
-//     defined by the knot sequence.
-//       * if ext=0, return the extrapolated value.
-//       * if ext=1, return 0
-//       * if ext=2, throw `out_of_range`
-//       * if ext=3, return the boundary value.
-//
+/// Evaluate a B-Spline or its derivatives.
+///
+/// @param [in] x
+///     An array of points at which to return the value of the smoothed
+///     spline or its derivatives.
+/// @param [in] tck
+///     The tuple returned by `splrep` containing the knots, coefficients,
+///     and degree of the spline.
+/// @param [in] der
+///     The order of derivative of the spline to compute (must be less than
+///      or equal to k).
+/// @param [in] ext
+///     Controls the value returned for elements of `x` not in the interval
+///     defined by the knot sequence.
+///       * if `0`, return the extrapolated value.
+///       * if `1`, return 0
+///       * if `2`, throw exception.
+///       * if `3`, return the boundary value.
+///
 template <class E, class... Args>
 auto splev(xexpression<E>& x, std::tuple<Args...>& tck, int der = 0, int ext = 0)
 {
@@ -98,11 +98,32 @@ class Spline
     virtual inline xtensor<double, 1> operator() (xtensor<double, 1>& x, int nu = 0) = 0;
 };
 
-// One-dimensional smoothing spline fit to a given set of data points.
-//
-class UnivariateSpline : public Spline
+/// One-dimensional smoothing spline fit to a given set of data points.
+///
+/// **Example usage:**
+/// @code
+/// #include <cmath>
+/// #include "xtensor/xtensor.hpp"
+/// #include "xtensor/xmath.hpp"
+/// #include "xtensor-interpolate/xinterpolate.hpp"
+///
+/// int main(void) {
+///     xt::xtensor<double, 1> x = xt::linspace(0, 2*M_PI, 100);
+///     xt::xtensor<double, 1> y = xt::sin(x);
+///
+///     auto s = xt::interpolate::UnivariateSpline(x, y).set_order(3).set_smoothing_factor(0.0);
+///
+///     xt::xtensor<double, 1> xs = xt::linspace(0, 2*M_PI, 1000);
+///     auto ys = s(xs);
+///
+///     return 0;
+/// }
+/// @endcode
+///
+class UnivariateSpline : public virtual Spline
 {
   protected:
+
     // Required interpolation parameters.
     xtensor<double, 1> x_;
     xtensor<double, 1> y_;
@@ -125,11 +146,18 @@ class UnivariateSpline : public Spline
     xtensor<int, 1> nrdata_;
 
   public:
-    // Construct a `UnivariateSpline` object.
-    //
-    // @param [in] x, y
-    //     The data points defining a curve y = f(x).
-    //
+
+    /// Construct a `UnivariateSpline` object.
+    ///
+    /// @param [in] x
+    ///     1-D array of input data. Must be increasing.
+    /// @param [in] y
+    ///     1-D array of input data corresponding to `x`. Must be the same length
+    ///     as `x`.
+    ///
+    /// @throws std::runtime_error
+    ///     Thrown if `x.shape()[0] != y.shape()[0]`.
+    ///
     inline UnivariateSpline(const xtensor<double, 1>& x, const xtensor<double, 1>& y)
         : x_(x)
         , y_(y)
@@ -145,12 +173,26 @@ class UnivariateSpline : public Spline
         XTENSOR_ASSERT(x_.shape()[0] == y_.shape()[0]);
     }
 
-    // Optional setters to be chained during object instantiation.
-    //
-    // Can also be used to alter spline object parameters after
-    // instantiation, however, this will require re-calculation of spline
-    // properties with FITPACK.
+    /// @name Attribute Setters
+    ///
+    /// Optional setters to be chained during object instantiation.
+    ///
+    /// Can also be used to alter spline object parameters after
+    /// instantiation, however, this will require re-calculation of spline
+    /// properties with *FITPACK*.
+    ///
+    /// @{
 
+    /// Set weights for spline fitting.
+    ///
+    /// @param [in] w
+    ///     1-D array of positive weights. Must have the same length as `x` and
+    ///     `y`. Defaults to an array of ones.
+    ///
+    /// @throws std::runtime_error
+    ///     Thrown if `w.shape()[0] != m` where `m` is the length of both `x`
+    ///     and `y`.
+    ///
     inline auto set_weights(const xtensor<double, 1>& w)
     {
         XTENSOR_ASSERT(w.shape()[0] == m_);
@@ -158,13 +200,28 @@ class UnivariateSpline : public Spline
         return reset(*this);
     }
 
+    /// Set the boundary of the approximation interval.
+    ///
+    /// @param [in] begin
+    ///     Defaults to `x[0]`.
+    /// @param [in] end
+    ///     Defaults to `x[x.shape()[0] - 1]`
+    ///
     inline auto set_bounds(double begin, double end)
     {
         xb_ = begin;
         xe_ = end;
         return reset(*this);
     }
-
+    
+    /// Set the degree of the smoothing spline.
+    ///
+    /// @param [in] k
+    ///     Degree of the smoothing spline. Defaults to `3`.
+    ///
+    /// @throws std::out_of_range
+    ///     Thrown if `k` does not satisfy `1 <= k <= 5`.
+    ///
     inline auto set_order(int k)
     {
         if (!(1 <= k && k <= 5))
@@ -175,12 +232,33 @@ class UnivariateSpline : public Spline
         return reset(*this);
     }
 
+    /// Set the positive smoothing factor used to choose the number of knots.
+    ///
+    /// @param [in] s
+    ///     Defaults to `w.shape()[0]`.
+    ///     If `0`, spline will interpolate through all data points.
+    ///
+    /// @throws std::out_of_range
+    ///     Thrown if `s < 0`.
+    ///
     inline auto set_smoothing_factor(double s)
     {
         s_ = s;
         return reset(*this);
     }
 
+    /// Set the extrapolation mode via integer.
+    ///
+    /// @param [in] ext
+    ///     Defaults to `0`. Options:
+    ///       - `0`: Return the extrapolated values.
+    ///       - `1`: Return 0.
+    ///       - `2`: Throw an exception.
+    ///       - `3`: Return the boundary value.
+    ///
+    /// @throws std::out_of_range
+    ///     Thrown if `ext` not recognized.
+    ///
     inline auto set_extrapolation_mode(int ext)
     {
         if (!(0 <= ext && ext <= 3))
@@ -191,6 +269,18 @@ class UnivariateSpline : public Spline
         return reset(*this);
     }
 
+    /// Set the extrapolation mode via string.
+    ///
+    /// @param [in] ext
+    ///     Defaults to `"extrapolate"`. Options:
+    ///       - `"extrapolate"`: Return the extrapolated values.
+    ///       - `"zeros"`: Return 0.
+    ///       - `"raise"`: Throw an exception.
+    ///       - `"const"`: Return the boundary value.
+    ///
+    /// @throws std::out_of_range
+    ///     Thrown if `ext` not recognized.
+    ///
     inline auto set_extrapolation_mode(const std::string& ext)
     {
         std::map<std::string, int> ext_map =
@@ -201,8 +291,20 @@ class UnivariateSpline : public Spline
         return reset(*this);
     }
 
-    // Evaluate the spline (or it's nu-th derivate) at positions given by x.
-    //
+    /// @}  // Attribute Setters
+
+    /// Evaluate the spline (or it's `nu`-th derivate) at positions given by x.
+    ///
+    /// @param [in] x
+    ///     An array of points at which to return the value of the smoothed
+    ///     spline or its derivatives.
+    /// @param [in] nu
+    ///     The order of derivative of the spline to compute (must be less than
+    ///      or equal to the spline order).
+    ///
+    /// @throw std::out_of_range
+    ///     Thrown if the requested derivative is invalid.
+    ///
     inline xtensor<double, 1> operator() (xtensor<double, 1>& x, int nu = 0)
     {
         if (!(nu <= k_))
@@ -218,8 +320,9 @@ class UnivariateSpline : public Spline
     }
 
   private:
-    // Evaluate `tck` based on spline parameters.
-    //
+
+    /// Evaluate `tck` based on spline parameters.
+    ///
     void evaluate(void)
     {
         nest_ = (s_) ? std::max(m_/2, 2*(k_+1)) : m_ + k_ + 1;
@@ -241,8 +344,8 @@ class UnivariateSpline : public Spline
         req_eval_ = false;
     }
 
-    // Get the `tck` tuple used for spline interpolation.
-    //
+    /// Get the `tck` tuple used for spline interpolation.
+    ///
     inline std::tuple<xtensor<double, 1>, xtensor<double, 1>, int> get_tck(void)
     {
         if (req_eval_)
@@ -252,8 +355,8 @@ class UnivariateSpline : public Spline
         return {t_, c_, k_};
     }
 
-    // Get a deinitialized version of the class.
-    //
+    /// Get a deinitialized version of the class.
+    ///
     inline UnivariateSpline& reset(const UnivariateSpline& self)
     {
         req_eval_ = true;
